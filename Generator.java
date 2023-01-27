5deb46a2da9553cbe8d0ace1279819aa2a4d9b02
root <root@Cody-Desktop.localdomain>
2023-01-27 15:06:25 -0500

    Creates a map generator file for the brick-breaker game

---
+++ Generator.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+
+public class Generator {
+    public int[][] map;
+    public int brickWidth;
+    public int brickHeight;
+    public Generator(int row, int col){
+        map = new int[row][col];
+        for(int[] mmap : map){
+            for(int j = 0; j < map[0].length; j++) {
+                mmap[j] = 1;
+            }
+        }
+        brickWidth = 540/col;
+        brickHeight = 540/col;
+    }
+
+    public void draw(Graphics2D g) {
+        for(int i = 0; i < map.length; i++){
+            for(int j = 0; j < map[0].length; j++){
+                if (map[i][j] > 0) {
+                    g.setColor(Color.RED);
+                    g.fillRect(j*brickWidth+80,i*brickHeight+50, brickWidth, brickHeight);
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor(Color.BLACK);
+                    g.drawRect(j*brickWidth+80, i*brickHeight+50, brickWidth, brickHeight);
+                }
+            }
+        }
+    }
+
+    public void setBrickValue(int value, int row, int col) {
+        map[row][col] = value;
+    }
+}
