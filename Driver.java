5ff64c5d2d900f0d578e02a62903c0f8386428c3
root <root@Cody-Desktop>
2023-01-27 19:56:31 -0500

    Makes user mouse transparent when in frame

--- Driver.java
+++ Driver.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
 
 
 /**
@@ -8,6 +11,13 @@ public class Driver {
     public static void main(String[] args){
         JFrame frame = new JFrame();
         BrickGame bg = new BrickGame();
+
+        // Creates a blank cursor
+        BufferedImage cursorImg = new BufferedImage(16,16, BufferedImage.TYPE_INT_ARGB);
+        Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0,0), "blank cursor");
+        frame.getContentPane().setCursor(blankCursor);
+
+
         frame.setBounds(10,10,700,600);
         frame.setTitle("Brick Breaker Game");
         frame.setResizable(false);
