40c8bb69ca806159abdec39023ed9d04e2d757a1
Cody Visscher <codyvisscher90@gmail.com>
2023-02-09 11:41:49 -0500

    Creates a class to hold a color of each brick for implementing power-ups

---
+++ Brick.java
@@ -0,0 +1,24 @@
+import java.awt.Color;
+import java.util.Random;
+
+public class Brick {
+    private Random rand = new Random();
+    private Color c;
+    public Brick() {
+        int color = rand.nextInt(100);
+        if(color < 80)
+            c = Color.red;
+        else if(color < 95)
+            c = Color.MAGENTA;
+        else
+            c = Color.BLUE;
+    }
+
+    public Color getColor() {
+        return c;
+    }
+
+    public void setColor(Color c) {
+        this.c = c;
+    }
+}
