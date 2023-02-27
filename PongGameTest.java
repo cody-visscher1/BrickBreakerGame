4ea58eb99bb888133fb6295482ce36b7f6fecc4e
Jacob Shoemaker <jacobshoemaker92@gmail.com>
2023-02-27 13:34:53 -0500

    Adds test constructors for moveLeft and moveRight

--- PongGameTest.java
+++ PongGameTest.java
@@ -1,7 +1,9 @@
+import org.junit.Before;
 import static org.junit.Assert.*;
 import org.junit.Test;
-
+import java.util.ArrayList;
 import java.text.DecimalFormat;
+import static org.junit.Assert.assertTrue;
 
 
 public class PongGameTest {
@@ -17,6 +19,31 @@ public class PongGameTest {
         assertEquals(0, (int)pg.getPlayerScore());
         assertNotNull("compGoal should not be null.", pg.getCompGoal());
         assertNotNull("playerGoal should not be null.", pg.getPlayerGoal());
+        assertEquals(0.75, pg.getDifficulty(), 0);
+
+
+    }
+
+    @Test
+    public void moveRightTestConstructor() {
+        pg= new PongGame();
+        pg.SetPlay(false);
+        pg. setPlayerX(20);
+        pg.moveRight();
+        assertTrue(pg.GetPlay());
+        assertEquals(40,pg.getPlayerX(),0);
+
+    }
+
+
+    @Test
+    public void moveLeftTestConstructor() {
+        pg= new PongGame();
+        pg.SetPlay(false);
+        pg.setPlayerX(20);
+        pg.moveLeft();
+        assertTrue(pg.GetPlay());
+        assertEquals(0,pg.getPlayerX(),0);
 
     }
 }
