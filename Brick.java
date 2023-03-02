7c3ef23355f78c46ca398e8a56afbbda3650e5af
cvissch <visschco@mail.gvsu.edu>
2023-03-01 22:06:43 -0500

    updates for checkstyle

--- Brick.java
+++ Brick.java
@@ -1,8 +1,20 @@
 import java.awt.Color;
 import java.util.Random;
 
+/**
+ * Class that is used to store the colors of the bricks.
+ *
+ * @author codyv, jacobshoe, brownky
+ */
 public class Brick {
+    /**
+     * The random object that allows the program to randomize the colors of the bricks.
+     */
     private Random rand = new Random();
+
+    /**
+     * The color of each brick that determines that bricks power up.
+     */
     private Color c;
 
     /**
@@ -11,23 +23,49 @@ public class Brick {
      */
     public Brick() {
         int color = rand.nextInt(100);
-        if(color < 90)
+        if(color < 90) {
             c = Color.WHITE;
-        else if(color < 95)
+        } else if(color < 95) {
             c = Color.MAGENTA;
-        else
+        } else {
             c = Color.BLUE;
+        }
     }
 
-    public Brick(Color c) {
-        this.c = c;
-    }
-
+    /**
+     * Allows the user to access the brick's color from other methods.
+     *
+     * @return the color of the brick.
+     */
     public Color getColor() {
         return c;
     }
 
-    public void setColor(Color c) {
+    /**
+     * Allows the user to set the brick's color from other methods.
+     *
+     * @param c - the color that the brick is being set to.
+     */
+    public void setColor(final Color c) {
         this.c = c;
+    }
+
+    /**
+     * Allows the user to access the Random objects from other methods
+     *
+     * @return the random object.
+     */
+    public Random getRand() {
+        return this.rand;
+    }
+
+    /**
+     * Allows the user to set the Random object from other methods.
+     *
+     * @param rand - the Random object that will be used to determine
+     *             the colors of the bricks.
+     */
+    public void setRand(final Random rand) {
+        this.rand = rand;
     }
 }
